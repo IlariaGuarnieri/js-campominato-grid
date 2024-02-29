9a7304e63b744db3137a658c0ea8eac91c85ff05
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-02-29 17:29:09 +0100

    [add] prima fase con square

---
+++ README.md
@@ -0,0 +1,19 @@
+Griglia Campo Minato
+===
+L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+# SVILUPPO
+
+1. seleziono il container e lo salvo in costante
+2. resetto
+3. ciclo di 100 ripetizioni
+4. ad ogni ciclo creo un quadrato e lo aggiungo alla griglia
+
+## CREAZIONE QUADRATO
+1. creare elemento html
+2. associo classe square
+3. associare il valore (n)
+4. associare un listener che al click mostri il valore e facciia il toggle della classe clicked
---
+++ script.js
@@ -0,0 +1,116 @@
+
+const gridContainer = document.querySelector('.grid-container')
+
+reset();
+
+for (let i = 1; i <=100 ; i++){
+  const square = getSquare(i);
+  console.log(square);
+  gridContainer.append(square)
+}
+
+
+// FUNCTIONS/////
+
+function getSquare(numero){
+  const sq = document.createElement('div');
+  sq.className = 'square';
+// cosi stampo i numeri
+  sq.innerHTML = numero;    
+  sq._sqID = numero;
+  sq.addEventListener('click', function(){
+    console.log(this._sqID);
+  })
+
+  return sq;
+}
+
+
+function reset(){
+  console.log(this);
+  gridContainer.innerHTML = '';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const gridContainer = document.querySelector('.grid-container')
+// const btnStart = document.querySelector('.btn-start')
+
+// btnStart.addEventListener('click', init);
+
+
+// function init(){
+//   reset();
+//   for(let i = 1; i<=100; i++){
+//     const square = getSquare(i);
+//     gridContainer.append(square)
+//   }
+// }
+
+// function getSquare(indice){
+//   const sq = document.createElement('div');
+//   sq.className = 'square';
+//   // creo una proprietà custom dell'elemento html e la chiamo _sqID
+//   sq.sqID = indice;
+
+
+//   // aggiungo il numero nel data attribute
+//   sq.dataset.sqid = indice
+
+//   sq.addEventListener('click' function(){
+//     const numero = this.sq_ID;
+//     this.innerHTML = (!this.classList.contains('clicked'))
+//                       ? this.innerHTML = numero     
+//                       : this.innerHTML = '';
+
+//     this.classList.toggle('clicked');
+//     this.classList.toggle(getOddEvenClass(numero));
+//     if(isEndGame()){
+//       reset();
+//       gridContainer.append(getBtnRestart());
+//     }
+//   })
+//   return sq;
+// }
+
+// function getBtnRestart(){
+//   // il gioco finisce se gli square con la classe clicked sono 100
+
+//   const clickedElements = document.querySelectorAll('clicked');
+//   return clickedElements === 64
+// }
+
+// function reset(){
+//   gridContainer.innerHTML = '';
+// }
+
+// function getOddEvenClass(n){
+//   if(n % 2 === 0){
+//     return 'even';
+//   }
+//   return 'odd';
+// }
