53b217097ad19e6f99287bb2e85f63b4c2bbb593
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-02-29 18:08:23 +0100

    [add] fine

--- script.js
+++ script.js
@@ -9,17 +9,31 @@ for (let i = 1; i <=100 ; i++){
   gridContainer.append(square)
 }
 
-
 // FUNCTIONS/////
 
 function getSquare(numero){
   const sq = document.createElement('div');
   sq.className = 'square';
 // cosi stampo i numeri
-  sq.innerHTML = numero;    
+// sq.innerHTML = numero;    
   sq._sqID = numero;
+
   sq.addEventListener('click', function(){
-    console.log(this._sqID);
+// this restituisce l'elemento contenitore
+    console.log(this);
+
+    // in questo caso uso la proprietà dataset
+    // const numero = this.dataset.squid;
+
+    // in questo caso uso proprietà custom che è più versatile
+    const numero =this._sqID;
+
+    this.innerHTML = (!this.classList.contains('clicked')) 
+                      ? this.innerHTML = numero
+                      : this.innerHTML = '';
+
+    this.classList.toggle('clicked');
+    this.classList.toggle(getOddEvenClass( numero ));
   })
 
   return sq;
@@ -30,6 +44,14 @@ function reset(){
   console.log(this);
   gridContainer.innerHTML = '';
 }
+
+function getOddEvenClass(n){
+  if(n % 2 === 0){
+    return 'even';
+  } 
+  return 'odd';
+}
+
 
 
 
